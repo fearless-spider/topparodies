22ec0a24eeb98018261a9807b311fd652acf3925
MegiTeam <admin@megiteam.pl>
2014-05-24 02:42:53 +0200

    Middleware to retrieve correct remote address from X-Forwarded-For

--- sitesngine/settings.py
+++ sitesngine/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'sitesngine.middleware.XForwardedForMiddleware',
 )
 
 ROOT_URLCONF = 'sitesngine.urls'
