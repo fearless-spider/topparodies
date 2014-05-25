9c0c0214f955bea4736fa1e1e3be71bbaaa3a795
Przemyslaw Pajak <bespider@gmail.com>
2014-05-25 12:29:49 +0200

    added djangostdio

--- sitesngine/settings.py
+++ sitesngine/settings.py
@@ -136,10 +136,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'OPTIONS': {
+    },
+    'OPTIONS': {
         'init_command': ("SET time_zone='%s'" % TIME_ZONE),
-        },
-    }
+    },
 }
 
 # Static files (CSS, JavaScript, Images)
