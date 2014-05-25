4dc118c75ef61731002d25222f64e54474ef883c
Przemyslaw Pajak <bespider@gmail.com>
2014-05-25 11:11:11 +0200

    added djangostdio

---
+++ sites/djangostdio_com/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from sitesngine.pages.views import MultiLanguagePageSitemap
+from zinnia.sitemaps import TagSitemap, EntrySitemap, AuthorSitemap, CategorySitemap
+
+admin.autodiscover()
+#admin.site._registry.update(admin.site._registry)
+
+sitemaps = {'tags': TagSitemap,
+            'blog': EntrySitemap,
+            'authors': AuthorSitemap,
+            'categories': CategorySitemap,
+            'pages':MultiLanguagePageSitemap,}
+
+urlpatterns = patterns('',
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^kurs/', include('zinnia.urls')),
+    url(r'^xmlrpc/$', 'django_xmlrpc.views.handle_xmlrpc'),
+    url(r'^comments/', include('django.contrib.comments.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.index',
+        {'sitemaps': sitemaps}),
+    url(r'^sitemap-(?P<section>.+)\.xml$', 'django.contrib.sitemaps.views.sitemap',
+        {'sitemaps': sitemaps}),
+    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^elfinder/', include('sitesngine.elfinder.urls')),
+    url(r'^', include('sitesngine.pages.urls')),
+)
