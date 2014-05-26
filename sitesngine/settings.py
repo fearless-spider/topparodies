0d5d3da29be6c5ff10213460e6997715cc540113
Przemyslaw Pajak <bespider@gmail.com>
2014-05-26 14:45:57 +0200

    new version of pages

--- sitesngine/settings.py
+++ sitesngine/settings.py
@@ -141,6 +141,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
+    'OPTIONS': {
+        'init_command': 'SET storage_engine=MyISAM',
+    }
 }
 
 # Static files (CSS, JavaScript, Images)
